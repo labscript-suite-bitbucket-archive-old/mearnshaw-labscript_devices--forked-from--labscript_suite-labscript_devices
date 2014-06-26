7a341467689f60bcff1ebcaf050b2d84bb415ece
Matt Earnshaw <matt@earnshaw.org.uk>
2014-06-26 16:39:41 +0100

    updated skeleton, compiles in runmanager

--- FPGADevice.py
+++ FPGADevice.py
@@ -5,19 +5,27 @@ from labscript_devices import labscript_device  #, BLACS_tab, BLACS_worker, runv
 from labscript import PseudoclockDevice, Pseudoclock, ClockLine, IntermediateDevice, Output, \
     LabscriptError  #DigitalQuantity, DigitalOut, DDS, config, LabscriptError
 
-# class design ?
-# FPGADevice('fpga', n_outputs, triggers...)
-# fpga.analog_out[0].ramp(...)
-# fpga.output('identifier').ramp(...)
+# Example
 #
-# FPGADevice('fpga', n_outputs, triggers...)
-# FPGAAnalogOut('analog0', fpga, 'ao0')
-# analog0.ramp(...)
+# import __init__ # only have to do this because we're inside the labscript directory
+# from labscript import *
+# from labscript_devices.FPGADevice import FPGADevice
+#
+# FPGADevice(name='fpga', n_outputs=2)
+# AnalogOut('analog0', fpga.outputs[0], 'ao0')
+# DigitalOut('digi0', fpga.outputs[1], 'digi1')
+#
+# start()
+# analog0.ramp(0, duration=3, initial=0, final=1, samplerate=1e4)
+# stop(1)
 
 
 @labscript_device
 class FPGADevice(PseudoclockDevice):
     """ device with n indiviually pseudoclocked outputs. """
+
+    clock_limit = 1e6  # true value??
+    clock_resolution = 1e-9  # true value??
 
     description = "FPGA-Device"
     allowed_children = [Pseudoclock]
@@ -31,15 +39,15 @@ class FPGADevice(PseudoclockDevice):
         self.outputs = []
         # Create the internal pseudoclocks and clocklines, and the outputs
         for n in range(n_outputs):
-            pc = Pseudoclock("fpga_pseudoclock{}".format(name), self, "clock_{}".format(n))
+            pc = Pseudoclock("fpga_pseudoclock{}".format(n), self, "clock_{}".format(n))
             self.pseudoclocks.append(pc)
 
             # Create the internal direct output clock_line
-            # FIXME: do we allow ramping?
-            cl = ClockLine("fpga_output{}_clock_line".format(n), pc, "internal", ramping_allowed=False)
+            cl = ClockLine("fpga_output{}_clock_line".format(n), pc, "fpga_internal{}".format(n))
             self.clocklines.append(cl)
 
             # Create the internal intermediate device (outputs) connected to the above clock line
+            # use OutputIntermediateDevice to limit no. outputs that can be connected?
             self.outputs.append(OutputIntermediateDevice("fpga_output_device{}".format(n), cl))
 
 
@@ -49,12 +57,11 @@ class OutputIntermediateDevice(IntermediateDevice):
     # clock_limit =
     # description = 
 
-    # FIXME: be more restrictive?
+    # be more restrictive?
     allowed_children = [Output]
 
-    def __init__(self, name, clock_line, output):
+    def __init__(self, name, clock_line):
         IntermediateDevice.__init__(self, name, clock_line)
-        self.output = Output(name.replace("_device",''), self, name.replace("device", "connection")
 
-    # disallow adding devices?
+    # disallow adding multiple devices?
     # def add_device(...):
