26357f9934f9ce8e986bde345d9d40641b66cfc8
Matt Earnshaw <matt@earnshaw.org.uk>
2014-07-09 11:42:46 +0100

    limits, real-time mode, handling multiple devices

--- fpga_api.py
+++ fpga_api.py
@@ -1,18 +1,18 @@
-""" API to communicate with FPGA over USB using defined protocol
+"""
+    API to communicate with FPGA over USB using defined protocol
     Requires libftdi.
 """
 
 import struct
+import inspect
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
 
-try:
-    import ftdi
-except ImportError:
-    import ftdi1 as ftdi
+import ftdi1 as ftdi
 
 
-def int_to_bytes(i, length=None):
+def value_to_bytes(i, length=None):
     """ Convert int to byte buffer, optionally padding to a specified length. """
     bytes_ = []
     while i > 0:
@@ -21,14 +21,21 @@ def int_to_bytes(i, length=None):
         i >>= 8
 
     if length is not None:
-        # add some padding null bytes at start
         diff = length - len(bytes_)
         bytes_[:0] = [0] * diff
 
     return ''.join(struct.pack('B', x) for x in bytes_)
 
-# FTDI status codes
-FT_OK = 0
+def quantize_analog_value(value, range_min, range_max):
+    """ DAC output specified by 16 bits with 0x0000 set to the
+    minimum of the range, 0xFFFF the maximum of the range (6 ranges
+    are possible with our LTC1592 DACs).
+
+    Returns value to be packed in the 16-bit data word to specify
+    the desired output given the currently programmed range. """
+    step = (range_max - range_min) / (2.0**16 - 1)
+    value = int(round((value - range_min) / step))
+    return value
 
 
 class FTDIError(Exception):
@@ -37,24 +44,36 @@ class FTDIError(Exception):
 
 def error_check(fn):
     """ decorator to check for and handle FTDI errors """
+
     def checked(*args):
-        ft_status = fn(*args)  # libftdi python bindings don't accept kwargs
-        if ft_status != FT_OK:
+        ret_val = fn(*args)  # libftdi python bindings don't accept kwargs
+
+        # some functions return a tuple with the return code as its first value
+        try:
+            ft_status = ret_val[0]
+        except TypeError:
+            ft_status = ret_val
+
+        # error values are negative
+        if ft_status < 0:
             try:
                 # assume the context is the first argument (...not aware of a counterexample)
                 err_msg = "An FTDI error occurred while calling '{}'".format(fn.__name__)
                 context = args[0]
-                ftdi_err_msg = ftdi.ftdi_get_error_string(context)
+                ftdi_err_msg = ftdi.get_error_string(context)
                 raise FTDIError("{}: {}".format(err_msg, ftdi_err_msg))
             except (IndexError, TypeError):
                 raise FTDIError(err_msg)
+        else:
+            return ret_val
+
     return checked
 
-
-# wrap functions in error checking/handling code
-ftdi.ftdi_usb_open = error_check(ftdi.ftdi_usb_open)
-ftdi.ftdi_usb_close = error_check(ftdi.ftdi_usb_close)
-ftdi.ftdi_set_bitmode = error_check(ftdi.ftdi_set_bitmode)
+# wrap ftdi functions in error checking/handling code
+ftdi_members = inspect.getmembers(ftdi, inspect.isfunction)
+for member in ftdi_members:
+    name, fn = member
+    vars(ftdi)[name] = error_check(fn)
 
 
 class FPGAInterface:
@@ -70,22 +89,43 @@ class FPGAInterface:
         self.product_id = product_id
 
         # create a new context
-        self.ftdi_c = ftdi.ftdi_new()
+        self.c = ftdi.new()
+        
+        # find all the attached devices with the given VID:PID
+        return_code, devices = ftdi.usb_find_all(self.c, vendor_id, product_id)
+
+        # get a python list of the discovered devices
+        device_list = []
+        while True:
+            try:
+                device_list.append(devices.dev)
+            except AttributeError:
+                raise FTDIError("No devices found, check connections.")
+
+            try:
+                devices.next()
+            except TypeError:
+                break
+                
+        # if there is more than one device found then warn about it
+        # later we should perhaps allow the user to choose the device in BLACS, or optionally specify a serial
+        if len(device_list) > 1:
+            return_code, manufacturer, description, serial = ftdi.usb_get_strings(self.c, device_list[0])
+            logging.warning("Multiple devices found with vendor id: {}, product id: {}. "
+                            "Opening device with serial number: {}".format(vendor_id, product_id, serial))
 
         # open first device with the supplied VID:PID
-        # FIXME: ok to assume 1 device or should we require more info (eg. serial) so we can choose ?
-        ftdi.ftdi_usb_open(self.ftdi_c, self.vendor_id, self.product_id)
-
-        # do EEPROM stuff
+        ftdi.usb_open_dev(self.c, device_list[0])
 
         # enter 245 synchronous FIFO mode with all bits set as outputs (0xFF)
-        ftdi.ftdi_set_bitmode(self.ftdi_c, 0xFF, ftdi.BITMODE_SYNCFF)
+        # assumes external EEPROM is set to 245 FIFO mode
+        ftdi.set_bitmode(self.c, 0xFF, ftdi.BITMODE_SYNCFF)
         
     # FIXME: make this class a proper context manager instead?
     def close(self):
         # close device and free context
-        ftdi.ftdi_usb_close(self.ftdi_c)
-        ftdi.ftdi_free(self.ftdi_c)
+        ftdi.usb_close(self.c)
+        ftdi.free(self.c)
 
     def __del__(self):
         # close device and free context when interface destroyed
@@ -94,7 +134,7 @@ class FPGAInterface:
     def send_bytes(self, bytes_):
         """ write a sequence of byte(s) to device. """
         logging.debug("writing {} bytes: {}".format(len(bytes_), repr(bytes_)))
-        n_bytes_written = ftdi.ftdi_write_data(self.ftdi_c, bytes_, len(bytes_))
+        n_bytes_written = ftdi.write_data(self.c, bytes_, len(bytes_))
 
         if n_bytes_written < len(bytes_):
             raise FTDIError("Problem writing to device, check connection - device may be closed?")
@@ -103,7 +143,7 @@ class FPGAInterface:
 
     def send_value(self, value, n_bytes=None):
         """ Send value, optionally coercing to a fit specified number of bytes. """
-        bytes_ = int_to_bytes(value, length=n_bytes)
+        bytes_ = value_to_bytes(value, length=n_bytes)
         return self.send_bytes(bytes_)
 
     def send_pseudoclock(self, board_number, channel_number, clock):
@@ -127,8 +167,11 @@ class FPGAInterface:
             self.send_value(tick['n_clocks'], n_bytes=4)
             self.send_value(tick['toggles'], n_bytes=4)
 
-    def send_analog_data(self, board_number, channel_number, data):
-        """ Send analog data to a given channel on a given board. """
+    def send_analog_data(self, board_number, channel_number, range_min, range_max, data):
+        """ Send analog data to a given channel on a given board. 
+        
+            range_min, range_max are the min/max output voltages configured on the DAC on this channel
+        """
         # send identifier
         self.send_value(self.data_mode_identifier, n_bytes=1)
 
@@ -147,10 +190,38 @@ class FPGAInterface:
         # send addressed data
         address = 0  # FIXME: how to deal with/specify addresses?
         for datum in data:
-            # pack data/address into 2 bytes
+            # pack data into 2 bytes
             self.send_value(address, n_bytes=2)
-            self.send_value(datum, n_bytes=2)
+
+            # pack quantized value into 2 bytes
+            self.send_value(quantize_analog_value(datum), n_bytes=2)
             address += 1  # FIXME: how to deal with/specify addresses?
+
+    def send_realtime_value(self, board_number, channel_number, value, range_min, range_max, output_type):
+        """ Send value to an output in real-time.
+            output_type is either 'analog' or 'digital'.
+
+            Returns the (possibly coerced/quantized) value sent to the board. """
+
+        # send mode identifier
+        self.send_value(self.realtime_mode_identifier, n_bytes=1)
+
+        # send board number
+        self.send_value(board_number, n_bytes=1)
+
+        # send channel number
+        self.send_value(channel_number, n_bytes=1)
+
+        if output_type == "analog":
+            value = quantize_analog_value(value, range_min, range_max)
+            self.send_value(value, n_bytes=2)
+        elif output_type == "digital":
+            # value is bool for digital outs but since bool is a subclass of int, send_value works.
+            self.send_value(value, n_bytes=1)
+        # error or log warning if unknown output_type?
+        
+        # return the value sent to the board
+        return value
 
     def start(self):
         """ Trigger a shot. """
@@ -162,4 +233,8 @@ class FPGAInterface:
 
     def stop(self):
         """ Stop output of board. """
+        pass
+
+    def reset(self):
+        """ Reset board. """
         pass
