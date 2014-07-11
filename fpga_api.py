41c8485ba7c979fcedaaf92bc962e28de488003e
Matt Earnshaw <matt@earnshaw.org.uk>
2014-07-11 16:03:01 +0100

    add parameter mode, make work with other versions of ftdi bindings

--- fpga_api.py
+++ fpga_api.py
@@ -9,7 +9,18 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-import ftdi1 as ftdi
+try:
+    import ftdi1 as ftdi
+except ImportError:
+    import ftdi
+    # in this version of the library method names prefixed with ftdi_
+    # rename them to exclude this prefix
+    ftdi_members = inspect.getmembers(ftdi)
+    for member in ftdi_members:
+        name, fn = member
+        new_name = name.replace("ftdi_", "")
+        vars(ftdi)[new_name] = fn
+        del vars(ftdi)[name]
 
 
 def value_to_bytes(i, length=None):
@@ -25,6 +36,7 @@ def value_to_bytes(i, length=None):
         bytes_[:0] = [0] * diff
 
     return ''.join(struct.pack('B', x) for x in bytes_)
+
 
 def quantize_analog_value(value, range_min, range_max):
     """ DAC output specified by 16 bits with 0x0000 set to the
@@ -56,8 +68,8 @@ def error_check(fn):
         except TypeError:
             ft_status = ret_val
 
-        # error values are negative
-        if ft_status < 0:
+        # error values are negative or NULL pointer (None)
+        if (ft_status < 0): # or (ft_status is None)
             try:
                 # assume the context is the first argument (...not aware of a counterexample)
                 err_msg = "An FTDI error occurred while calling '{}'".format(fn.__name__)
@@ -72,10 +84,14 @@ def error_check(fn):
     return checked
 
 # wrap ftdi functions in error checking/handling code
+# our error checking code calls get_error_string so don't wrap it- could cause infinite recursion
+# also don't wrap the functions returning void, no way to determine their success
+exclude = ("get_error_string", "list_free", "list_free2", "set_usbdev", "deinit", "free", "set_ft232h_cbus")
 ftdi_members = inspect.getmembers(ftdi, inspect.isfunction)
 for member in ftdi_members:
     name, fn = member
-    vars(ftdi)[name] = error_check(fn)
+    if name not in exclude:
+        vars(ftdi)[name] = error_check(fn)
 
 
 class FPGAInterface:
@@ -87,7 +103,7 @@ class FPGAInterface:
     trigger_mode_identifier = 4
 
     # FIXME: change to the correct VID:PID for FT232H
-    def __init__(self, vendor_id=0x0403, product_id=0x6001):
+    def __init__(self, vendor_id=0x0403, product_id=0x6014):  # product_id=0x6001):
         self.vendor_id = vendor_id
         self.product_id = product_id
 
@@ -118,10 +134,18 @@ class FPGAInterface:
 
         # open first device with the supplied VID:PID
         ftdi.usb_open_dev(self.c, device_list[0])
+        #ftdi.usb_open(self.c, 0x0403, 0x6014)
 
+        #ftdi.set_bitmode(self.c, 0xFF, ftdi.BITMODE_SYNCFF)
+        self.init_chip()
+
+    def init_chip(self, bitmode=ftdi.BITMODE_SYNCFF):
         # enter 245 synchronous FIFO mode with all bits set as outputs (0xFF)
         # assumes external EEPROM is set to 245 FIFO mode
-        ftdi.set_bitmode(self.c, 0xFF, ftdi.BITMODE_SYNCFF)
+        ftdi.set_latency_timer(self.c, 2)
+        ftdi.set_bitmode(self.c, 0xFF, ftdi.BITMODE_RESET)
+        ftdi.usb_purge_buffers(self.c)
+        ftdi.set_bitmode(self.c, 0xFF, bitmode)
 
     # FIXME: make this class a proper context manager instead?
     def close(self):
@@ -129,14 +153,17 @@ class FPGAInterface:
         ftdi.usb_close(self.c)
         ftdi.free(self.c)
 
+    """
     def __del__(self):
         # close device and free context when interface destroyed
         self.close()
+    """
 
     def send_bytes(self, bytes_):
         """ write a sequence of byte(s) to device. """
         logging.debug("writing {} bytes: {}".format(len(bytes_), repr(bytes_)))
         n_bytes_written = ftdi.write_data(self.c, bytes_, len(bytes_))
+        #n_bytes_written = ftdi.transfer_data_done(ftdi.write_data_submit(self.c, bytes_, len(bytes_)))
 
         if n_bytes_written < len(bytes_):
             raise FTDIError("Problem writing to device, check connection - device may be closed?")
@@ -170,8 +197,8 @@ class FPGAInterface:
             self.send_value(tick['toggles'], n_bytes=4)
 
     def send_analog_data(self, board_number, channel_number, range_min, range_max, data):
-        """ Send analog data to a given channel on a given board. 
-        
+        """ Send analog data to a given channel on a given board.
+
             range_min, range_max are the min/max output voltages configured on the DAC on this channel
         """
         # send identifier
@@ -222,9 +249,25 @@ class FPGAInterface:
             # value is bool for digital outs but since bool is a subclass of int, send_value works.
             self.send_value(value, n_bytes=1)
         # error or log warning if unknown output_type?
-        
+
         # return the value sent to the board
         return value
+
+    def send_parameter(self, board_number, channel_number, value):
+        """ Update a parameter on an output. """
+        # FIXME: need more info about the possible parameters etc.
+
+        # send mode identifier
+        self.send_value(self.parameter_mode_identifier, n_bytes=1)
+
+        # send board number
+        self.send_value(board_number, n_bytes=1)
+
+        # send channel number
+        self.send_value(channel_number, n_bytes=1)
+
+        # send the parameter
+        self.send_value(value, n_bytes=1)
 
     def start(self):
         """ Trigger a shot. """
@@ -240,4 +283,8 @@ class FPGAInterface:
 
     def reset(self):
         """ Reset board. """
-        pass
+        logging.info("Resetting chip.")
+        ftdi.set_bitmode(self.c, 0xFF, ftdi.BITMODE_RESET)
+        ftdi.usb_reset(self.c)
+        ftdi.usb_purge_buffers(self.c)
+        logging.info("Reset chip.")
